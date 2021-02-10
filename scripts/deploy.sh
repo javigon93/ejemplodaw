427af95d6230b177182fb2c15eef89f4f8b9c617
Javier Gonzalez <javigon93@gmail.com>
2021-02-10 17:36:54 +0100

    correcccion

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,11 @@ sudo docker container stop apache_javier_gonzalez
 sudo docker container rm apache_javier_gonzalez
 sudo docker container stop mariadb_javier_gonzalez
 sudo docker container rm mariadb_javier_gonzalez
+sudo rm -r /opt/javier_gonzalez/docker/apache/*
 sudo cp -a ./dist/* /opt/javier_gonzalez/docker/apache
+sudo docker container exec -it apache_javier_gonzalez docker-php-ext-install pdo pdo_mysql
+sudo docker container exec -it apache_javier_gonzalez a2enmod rewrite
+sudo docker container restart apache_javier_gonzalez
 sudo .//opt/javier_gonzalez/scripts/apache_javier.sh
 sudo .//opt/javier_gonzalez/scripts/mariadb_javier.sh
 
