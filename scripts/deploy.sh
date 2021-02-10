a94ec3df1079c73be8bf018b46bc2bbd077e4143
Javier Gonzalez <javigon93@gmail.com>
2021-02-10 19:12:08 +0100

    correcccion

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,7 @@
 
 sudo rm -r ./dist/*
 sudo cp -a ./src/web/* ./dist
-sudo npm install
-sudo node-sass -o /css /scss/main.scss
+sudo sass /scss/main.scss:/css
 sudo docker container stop apache_javier_gonzalez
 sudo docker container rm apache_javier_gonzalez
 sudo docker container stop mariadb_javier_gonzalez
