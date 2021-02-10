c60ab27e62766b7d49bac353e2379a8d0e25f879
Javier Gonzalez <javigon93@gmail.com>
2021-02-10 18:08:49 +0100

    correcccion

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,15 @@
 
 sudo rm -r ./dist/*
 sudo cp -a ./src/web/* ./dist
-sudo node-sass -o dist/css/ dist/scss/main.scss
+sudo sass -o dist/css/ dist/scss/main.scss
 sudo docker container stop apache_javier_gonzalez
 sudo docker container rm apache_javier_gonzalez
 sudo docker container stop mariadb_javier_gonzalez
 sudo docker container rm mariadb_javier_gonzalez
 sudo rm -r /opt/javier_gonzalez/docker/apache/*
 sudo cp -a ./dist/* /opt/javier_gonzalez/docker/apache
+sudo bash /opt/javier_gonzalez/scripts/apache_javier.sh
+sudo bash /opt/javier_gonzalez/scripts/mariadb_javier.sh
 sudo docker container exec -it apache_javier_gonzalez docker-php-ext-install pdo pdo_mysql
 sudo docker container exec -it apache_javier_gonzalez a2enmod rewrite
 sudo docker container restart apache_javier_gonzalez
-sudo .//opt/javier_gonzalez/scripts/apache_javier.sh
-sudo .//opt/javier_gonzalez/scripts/mariadb_javier.sh
-
