4b662410ef571127bfab9beaad573211e8fd03d6
Javier Gonzalez <javigon93@gmail.com>
2021-02-10 16:52:45 +0100

    añadido proyecto

---
+++ src/web/index.php
@@ -0,0 +1,38 @@
+<?php
+
+try {
+require __DIR__ . '/vendor/autoload.php';
+
+//librería para poder leer archivos.env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+try {
+    $pdo = new \PDO($_ENV['DB_CONNECTION'] . ":host=" . 
+        $_ENV['DB_HOST'].";port=".$_ENV['DB_PORT'].";dbname=" . $_ENV['DB_DATABASE'], 
+        $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+    $pdo->exec("set names utf8");
+    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "HOLA".$e->getMessage();
+    exit();
+}
+
+$sql = "SELECT * FROM info ";
+
+$params = [
+    ":id" => 2
+];
+
+$ps = $pdo->prepare($sql);
+$ps->execute($params);
+$result = $ps->columnCount()>0? $ps->fetchAll(\PDO::FETCH_ASSOC): $ps->rowCount(); 
+
+echo '<h1>Resultado de la base de datos'.$_ENV['DB_DATABASE'].'</h1>';
+echo '<p>Nombre: '.$result[0]['nombre'].'</p>';
+echo '<p>Apellido: '.$result[0]['apellido'].'</p>';
+echo '<p>Edad: '.$result[0]['edad'].'</p>';
+} catch (Exception $e) {
+    
+    echo $e->getMessage();
+}
