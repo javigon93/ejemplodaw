8fb249bd4ff303c34ce7f7f5576e8694f7b053bd
Javier Gonzalez <javigon93@gmail.com>
2021-02-11 08:29:41 +0100

    correcccion

--- src/web/index.php
+++ src/web/index.php
@@ -1,38 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='./css/main.css'>
+</head>
 <?php
 
 try {
-require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
-//librería para poder leer archivos.env
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+    //librería para poder leer archivos.env
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
-try {
-    $pdo = new \PDO($_ENV['DB_CONNECTION'] . ":host=" . 
-        $_ENV['DB_HOST'].";port=".$_ENV['DB_PORT'].";dbname=" . $_ENV['DB_DATABASE'], 
-        $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
-    $pdo->exec("set names utf8");
-    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "HOLA".$e->getMessage();
-    exit();
-}
+    try {
+        $pdo = new \PDO(
+            $_ENV['DB_CONNECTION'] . ":host=" .
+                $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_DATABASE'],
+            $_ENV['DB_USERNAME'],
+            $_ENV['DB_PASSWORD']
+        );
+        $pdo->exec("set names utf8");
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "HOLA" . $e->getMessage();
+        exit();
+    }
 
-$sql = "SELECT * FROM info ";
+    $sql = "SELECT * FROM info ";
 
-$params = [
-    ":id" => 2
-];
+    $params = [
+        ":id" => 2
+    ];
 
-$ps = $pdo->prepare($sql);
-$ps->execute($params);
-$result = $ps->columnCount()>0? $ps->fetchAll(\PDO::FETCH_ASSOC): $ps->rowCount(); 
+    $ps = $pdo->prepare($sql);
+    $ps->execute($params);
+    $result = $ps->columnCount() > 0 ? $ps->fetchAll(\PDO::FETCH_ASSOC) : $ps->rowCount();
 
-echo '<h1>Resultado de la base de datos'.$_ENV['DB_DATABASE'].'</h1>';
-echo '<p class="c-texto" >Nombre: '.$result[0]['nombre'].'</p>';
-echo '<p>Apellido: '.$result[0]['apellido'].'</p>';
-echo '<p>Edad: '.$result[0]['edad'].'</p>';
+    echo '<h1>Resultado de la base de datos' . $_ENV['DB_DATABASE'] . '</h1>';
+    echo '<p class="c-texto" >Nombre: ' . $result[0]['nombre'] . '</p>';
+    echo '<p>Apellido: ' . $result[0]['apellido'] . '</p>';
+    echo '<p>Edad: ' . $result[0]['edad'] . '</p>';
 } catch (Exception $e) {
-    
+
     echo $e->getMessage();
 }
