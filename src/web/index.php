36aa76721149eb9bac76648c2c83b136d50dee20
Javier Gonzalez <javigon93@gmail.com>
2021-02-11 08:33:00 +0100

    correcccion

--- src/web/index.php
+++ src/web/index.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' type='text/css' media='screen' href='./css/main.css'>
 </head>
+<body>
+    
+
 <?php
 
 try {
@@ -48,3 +51,6 @@ try {
 
     echo $e->getMessage();
 }
+
+?>
+</body>
